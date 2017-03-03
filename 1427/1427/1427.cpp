7030037e9b3e6232d5d96e0cb65f962c89b93b9e
sarahcheng0122 <sarahcheng0122@gmail.com>
2017-03-03 09:55:42 +0800

    練習7

--- 1427/1427/1427.cpp
+++ 1427/1427/1427.cpp
@@ -1,11 +1,28 @@
 // 1427.cpp: 主要專案檔。
 
 #include "stdafx.h"
-
+#include <string>
+#include <iostream>
 using namespace System;
 
 int main(array<System::String ^> ^args)
 {
     Console::WriteLine(L"Hello World");
     return 0;
+}
+
+
+
+
+
+bool Guess(int number) {
+	return false;
+}
+int main(array<System::String ^> ^args)
+{
+	int guess;
+	do {
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
 }
